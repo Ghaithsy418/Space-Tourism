030840028ee75463e095b83934fbe840be7618dc
Ghaithsy418 <ghaithsy418@gmail.com>
2025-03-08 22:14:25 +0300

    route fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,4 @@
-import {
-  Navigate,
-  Route,
-  Routes,
-  useLocation,
-} from "react-router-dom";
-import HomePage from "./Components/HomePage";
-import Destination from "./Components/Destination";
-import Crew from "./Components/Crew";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import Technology from "./Components/Technology";
 import Moon from "./Components/Moon";
 import Mars from "./Components/Mars";
@@ -19,40 +11,47 @@ import Crew04 from "./Components/Crew04";
 import Technology01 from "./Components/Technology01";
 import Technology02 from "./Components/Technology02";
 import Technology03 from "./Components/Technology03";
-import Applayout from "./Components/Applayout";
 import { AnimatePresence } from "motion/react";
+import { lazy } from "react";
+
+const AppLayout = lazy(() => import("./Components/Applayout"));
+const WrongMessage = lazy(() => import("./Components/WrongMessage"));
+const Destination = lazy(() => import("./Components/Destination"));
+const Crew = lazy(() => import("./Components/Crew"));
+const HomePage = lazy(() => import("./Components/HomePage"));
 
 function App() {
   const location = useLocation();
   return (
-      <AnimatePresence mode="wait">
-        <Routes location={location} key={location.key}>
-          <Route element={<Applayout />}>
-            <Route index element={<Navigate replace to="home" />} />
-            <Route path="home" element={<HomePage />} />
-            <Route path="destination" element={<Destination />}>
-              <Route index element={<Navigate replace to="moon" />} />
-              <Route path="moon" element={<Moon />} />
-              <Route path="mars" element={<Mars />} />
-              <Route path="eruopa" element={<Europa />} />
-              <Route path="titan" element={<Titan />} />
-            </Route>
-            <Route path="crew" element={<Crew />}>
-              <Route index element={<Navigate replace to="crew1" />} />
-              <Route path="crew1" element={<Crew01 />} />
-              <Route path="crew2" element={<Crew02 />} />
-              <Route path="crew3" element={<Crew03 />} />
-              <Route path="crew4" element={<Crew04 />} />
-            </Route>
-            <Route path="technology" element={<Technology />}>
-              <Route index element={<Navigate replace to="tech1" />} />
-              <Route path="tech1" element={<Technology01 />} />
-              <Route path="tech2" element={<Technology02 />} />
-              <Route path="tech3" element={<Technology03 />} />
-            </Route>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.key}>
+        <Route path="*" element={<WrongMessage />} />
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="home" />} />
+          <Route path="home" element={<HomePage />} />
+          <Route path="destination" element={<Destination />}>
+            <Route index element={<Navigate replace to="moon" />} />
+            <Route path="moon" element={<Moon />} />
+            <Route path="mars" element={<Mars />} />
+            <Route path="eruopa" element={<Europa />} />
+            <Route path="titan" element={<Titan />} />
           </Route>
-        </Routes>
-      </AnimatePresence>
+          <Route path="crew" element={<Crew />}>
+            <Route index element={<Navigate replace to="crew1" />} />
+            <Route path="crew1" element={<Crew01 />} />
+            <Route path="crew2" element={<Crew02 />} />
+            <Route path="crew3" element={<Crew03 />} />
+            <Route path="crew4" element={<Crew04 />} />
+          </Route>
+          <Route path="technology" element={<Technology />}>
+            <Route index element={<Navigate replace to="tech1" />} />
+            <Route path="tech1" element={<Technology01 />} />
+            <Route path="tech2" element={<Technology02 />} />
+            <Route path="tech3" element={<Technology03 />} />
+          </Route>
+        </Route>
+      </Routes>
+    </AnimatePresence>
   );
 }
 
---
+++ src/Components/WrongMessage.tsx
@@ -0,0 +1,9 @@
+function WrongMessage() {
+  return (
+    <div className="flex items-center justify-center mt-[45vh]">
+      <h2 className="text-7xl text-gray-400">Page Not Found :(</h2>
+    </div>
+  );
+}
+
+export default WrongMessage;
